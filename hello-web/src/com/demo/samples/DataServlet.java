e7b0cae06aec881ade4292bf07c2d6eff8eb1720
Bo Fan <22100041+bfan4@users.noreply.github.com>
2019-05-13 21:00:51 -0400

    Add files via upload

---
+++ hello-web/src/com/demo/samples/DataServlet.java
@@ -0,0 +1,61 @@
+package com.demo.samples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DataServlet
+ */
+@WebServlet("/DataServlet")
+public class DataServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public DataServlet() {
+        super();
+        
+    }
+    
+    public void init() throws ServletException {
+    	
+    	try {
+    		Class.forName()
+    	}catch() {
+    		
+    	}
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		out.println("<table>");
+		out.println("<tr>");
+		try {
+			PreparedStatement ps = con.prepareStatement("select * from users");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
